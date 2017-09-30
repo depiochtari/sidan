f0cf20cfd562a6d144f6d966417afc5996f8e87b
depiochtari <32217693+depiochtari@users.noreply.github.com>
2017-10-01 12:49:31 +1400

    tak beraturan tabel alamat

--- app/Nelayan.php
+++ app/Nelayan.php
@@ -8,7 +8,6 @@ class Nelayan extends Model
 {
     protected $table = 'nelayan';
 	
-<<<<<<< HEAD
 	protected $fillable = ['nama_nelayan'];
 
 }
@@ -18,8 +17,6 @@ class Alamat extends Model
 	protected $table = 'alamat';
 
 	protected $fillable = ['alamat_nelayan'];
-=======
->>>>>>> 8b63d6738d2149a2f9856ce3e11c417fae677d11
 	protected $fillable = ['nama', 'alamat'];
 
 }
