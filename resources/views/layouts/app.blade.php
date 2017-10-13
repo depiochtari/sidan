1d2a135cf38347c323352646a344f81289643938
ImamTop <imamtaufiqurrokhman@gmail.com>
2017-10-13 10:11:07 +0700

    search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/datatables.min.css') }}">
 
     <style>
         body {
@@ -102,8 +102,13 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script src="{{ url('js/jquery.min.js') }}"></script>
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('js/datatables.min.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready( function () {
+            $('#datatable').DataTable();
+        } );
+    </script>
 </body>
 </html>
--- resources/views/nelayan/index.blade.php
+++ resources/views/nelayan/index.blade.php
@@ -7,41 +7,45 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Data Nelayan</div>
                 <div class="panel-body">
-                    <table class="table table-condensed">
-                        <tr>
-                            <td>ID</td>
-                            <td>KN</td>
-                            <td>Nama</td>
-                            <td>Alamat</td>
-                            <td>Status</td>
-                            <td>KUB</td>
-                            <td>HP</td>
-                            <td>BANK</td>
-                            <td>No Rekening</td>
-                            <td>Edit</td>
-                            <td>Hapus</td>
-                        </tr>
-                        @foreach($datanelayan as $nelayan)
-                        <tr>
-                            <td>{{ $nelayan->id }}</td>
-                            <td>{{ $nelayan->kn }}</td>
-                            <td>{{ $nelayan->nama }}</td>
-                            <td>{{ $nelayan->alamat }}</td>
-                            <td>{{ $nelayan->status }}</td>
-                            <td>{{ $nelayan->kub }}</td>
-                            <td>{{ $nelayan->hp }}</td>
-                            <td>{{ $nelayan->bank }}</td>
-                            <td>{{ $nelayan->norek }}</td>
-                            <td><a href="{{ url('nelayan/'.$nelayan->id.'/edit') }}">Edit</a></td>
-                            <td>
-                                <form action="{{ url('nelayan/'.$nelayan->id) }}" method="POST">
-                                    {{ csrf_field() }}
-                                    {{ method_field('DELETE') }}
-                                    <button type="submit" class="btn btn-danger">Hapus</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
+                    <table id="datatable" class="table table-condensed">
+                        <thead>
+                            <th>
+                                <td>ID</td>
+                                <td>KN</td>
+                                <td>Nama</td>
+                                <td>Alamat</td>
+                                <td>Status</td>
+                                <td>KUB</td>
+                                <td>HP</td>
+                                <td>BANK</td>
+                                <td>No Rekening</td>
+                                <td>Edit</td>
+                                <td>Hapus</td>
+                            </th>
+                        </thead>
+                        <tbody>
+                            @foreach($datanelayan as $nelayan)
+                                <tr>
+                                    <td>{{ $nelayan->id }}</td>
+                                    <td>{{ $nelayan->kn }}</td>
+                                    <td>{{ $nelayan->nama }}</td>
+                                    <td>{{ $nelayan->alamat }}</td>
+                                    <td>{{ $nelayan->status }}</td>
+                                    <td>{{ $nelayan->kub }}</td>
+                                    <td>{{ $nelayan->hp }}</td>
+                                    <td>{{ $nelayan->bank }}</td>
+                                    <td>{{ $nelayan->norek }}</td>
+                                    <td><a href="{{ url('nelayan/'.$nelayan->id.'/edit') }}">Edit</a></td>
+                                    <td>
+                                        <form action="{{ url('nelayan/'.$nelayan->id) }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button type="submit" class="btn btn-danger">Hapus</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
